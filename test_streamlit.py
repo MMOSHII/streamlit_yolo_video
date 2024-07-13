7ce076f326c3bce032ce1811d86038ea4c9686be
MMOSHII <94529585+MMOSHII@users.noreply.github.com>
2024-07-13 14:57:07 +0700

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -23,46 +23,30 @@ def app():
         input_path = os.path.join(os.getcwd(), f"temp_{unique_id}.mp4")
         output_path = os.path.join(os.getcwd(), f"output_{unique_id}.mp4")
 
-        try:
+       try:
             with open(input_path, "wb") as temp_file:
                 temp_file.write(uploaded_file.read())
 
-            video_stream = cv2.VideoCapture(input_path)
-            width = int(video_stream.get(cv2.CAP_PROP_FRAME_WIDTH)) 
-            height = int(video_stream.get(cv2.CAP_PROP_FRAME_HEIGHT)) 
-            fourcc = cv2.VideoWriter_fourcc(*'h264') 
-            fps = int(video_stream.get(cv2.CAP_PROP_FPS)) 
+            image = cv2.imread(input_path)
 
-            out_video = cv2.VideoWriter(output_path, int(fourcc), fps, (width, height)) 
+            result = model(image)
+            for detection in result[0].boxes.data:
+                x0, y0 = (int(detection[0]), int(detection[1]))
+                x1, y1 = (int(detection[2]), int(detection[3]))
+                score = round(float(detection[4]), 2)
+                cls = int(detection[5])
+                object_name = model.names[cls]
+                label = f'{object_name} {score}'
 
-            with st.spinner('Processing video...'): 
-                while True:
-                    ret, frame = video_stream.read()
-                    if not ret:
-                        break
-                    result = model(frame)
-                    for detection in result[0].boxes.data:
-                        x0, y0 = (int(detection[0]), int(detection[1]))
-                        x1, y1 = (int(detection[2]), int(detection[3]))
-                        score = round(float(detection[4]), 2)
-                        cls = int(detection[5])
-                        object_name =  model.names[cls]
-                        label = f'{object_name} {score}'
+                if object_name in selected_objects and score > min_confidence:
+                    cv2.rectangle(image, (x0, y0), (x1, y1), (255, 0, 0), 2)
+                    cv2.putText(image, label, (x0, y0 - 10),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
-                        if model.names[cls] in selected_objects and score > min_confidence:
-                            cv2.rectangle(frame, (x0, y0), (x1, y1), (255, 0, 0), 2)
-                            cv2.putText(frame, label, (x0, y0 - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-                        else:
-                            continue
-                    
-                    detections = result[0].verbose()
-                    cv2.putText(frame, detections, (10, 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  
-                    out_video.write(frame) 
-            video_stream.release()
-            out_video.release()
+            cv2.imwrite(output_path, image)
+
+            # Display the processed image
+            st.image(output_path)
 
             # Delete temporary files after processing
             if os.path.exists(input_path):
